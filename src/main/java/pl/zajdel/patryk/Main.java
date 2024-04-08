d30345de7c83c2cc983c966d152e1b0e76c51df8
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-04-08 14:07:13 +0200

    Implement resources verifying

--- src/main/java/pl/zajdel/patryk/Main.java
+++ src/main/java/pl/zajdel/patryk/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/validPolicy.json");
-        System.out.println(RolePolicy.verifyJsonFormat(file, "role-policy-schema.json"));
+        RolePolicy rolePolicy = RolePolicy.fromJsonWithGivenSchema(file, "role-policy-schema.json");
+        System.out.println(rolePolicy.verifyResources());
     }
 }
--- src/main/java/pl/zajdel/patryk/RolePolicy.java
+++ src/main/java/pl/zajdel/patryk/RolePolicy.java
@@ -8,10 +8,13 @@ import com.networknt.schema.SpecVersion;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class RolePolicy {
-    public static boolean verifyResources(String json) {
-        return false;
+    private final JsonNode policy;
+
+    private RolePolicy(JsonNode policy) {
+        this.policy = policy;
     }
 
     public static boolean verifyJsonFormat(File jsonFile, String schemaName) throws IOException {
@@ -19,5 +22,52 @@ public class RolePolicy {
         JsonSchema schema = schemaFactory.getSchema(RolePolicy.class.getResourceAsStream("/" + schemaName));
         JsonNode jsonNode = new ObjectMapper().readTree(jsonFile);
         return schema.validate(jsonNode).isEmpty();
+    }
+
+    public static RolePolicy fromJsonWithGivenSchema(File jsonFile, String schemaName) throws IOException, IllegalArgumentException {
+        if (verifyJsonFormat(jsonFile, schemaName)) {
+            return new RolePolicy(new ObjectMapper().readTree(jsonFile));
+        }
+        throw new IllegalArgumentException("Invalid policy");
+    }
+
+    public boolean verifyResources() {
+        List<JsonNode> resources = getResources();
+        boolean result = true;
+
+        for (JsonNode resource : resources) {
+            result = categorizeResources(resource);
+            if (!result) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean categorizeResources(JsonNode resource) {
+        if (resource.isArray()) {
+            return verifyResourceDefinedAsArray(resource);
+        }
+        return verifyResourceDefinedAsString(resource);
+    }
+
+    private boolean verifyResourceDefinedAsArray(JsonNode resource) {
+        for (JsonNode node : resource) {
+            if (node.toString().equals("\"*\"")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean verifyResourceDefinedAsString(JsonNode resource) {
+        return !resource.toString().equals("\"*\"");
+    }
+
+    private List<JsonNode> getResources() {
+        JsonNode PolicyDocument = policy.get("PolicyDocument");
+        JsonNode Statement = PolicyDocument.get("Statement");
+        return Statement.findValues("Resource");
     }
 }
